0d27c0469c68e8ea7dd4066392eadcd0f709d747
Michael Holmes <michaelholmes618@gmail.com>
2021-04-11 13:38:31 -0500

    completion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { add, multiply, subtract, divide, pow } from "./calculator";
 
 //Import the add, multiply, subtract, power, and divide functions
 //from the calculator.js file.
@@ -11,6 +12,7 @@ ReactDOM.render(
     <li>{multiply(2, 3)}</li>
     <li>{subtract(7, 2)}</li>
     <li>{divide(5, 2)}</li>
+    <li>{pow(3, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
